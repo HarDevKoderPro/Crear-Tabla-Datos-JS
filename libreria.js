35787ff9c8a0ff01cbd8d14fdbe1ab8e260cd585
HardevKoder <hardevkoder@gmail.com>
2025-03-06 12:35:31 -0500

    Separo fucionalidades Crear y Llenar

--- libreria.js
+++ libreria.js
@@ -1,25 +1,26 @@
 "use strict";
 export class Libreria {
   //-------------------------------------------------------
-  // TABLA PARA MOSTRAR DATOS
+  // TABLA PARA MOSTRAR DATOS (Sin Datos)
   //-------------------------------------------------------
-  // Recibe un Objeto con los Parámetros
-  static crearTablaDatos(params) {
+  static crearTablaDatos(id, tituloTabla, camposArr, padre) {
+
     // Creo el elemento principal (Tabla)
     const tabla = document.createElement("table");
-    tabla.id = params.id;
-    tabla.className = params.id;
+    tabla.id = id;
+    tabla.classList.add(id);
 
     // Titulo de la tabla
     const titulo = document.createElement("caption");
-    titulo.className = "tituloTabla";
-    titulo.textContent = params.tituloTabla;
+    titulo.id = "tituloTabla";
+    titulo.classList.add("tituloTabla");
+    titulo.textContent = tituloTabla;
     tabla.appendChild(titulo);
 
     // Encabezados de la tabla (Campos)
     const encabezado = document.createElement("thead");
     const encabezadoFila = document.createElement("tr");
-    params.campos.forEach((campo) => {
+    camposArr.forEach((campo) => {
       encabezadoFila.innerHTML += `
       <th class = "tablaCampos"> ${campo}</th>`;
     });
@@ -29,20 +30,36 @@ export class Libreria {
     // Cuerpo de la tabla
     const cuerpo = document.createElement("tbody");
     cuerpo.id = "tablaResultados";
-    cuerpo.className = "tablaResultados";
-    params.datos.forEach((dato) => {
-      const fila = document.createElement("tr");
-      fila.innerHTML += `
-        <td>${dato.id}</td>
-        <td>${dato.nombre}</td>
-        <td>${dato.apellido}</td>
-        <td>${dato.edad}</td>
-      `;
-      cuerpo.appendChild(fila);
-    });
+    cuerpo.classList.add("tablaResultados");
     tabla.appendChild(cuerpo);
 
     // Agrego la tabla al contenedor padre y la retorno
-    document.querySelector(params.padre).appendChild(tabla);
+    document.querySelector(padre).appendChild(tabla);
+
+    // Retorno la Referencia de la tabla
+    return tabla;
+  }
+
+  //-------------------------------------------------------
+  //2- LLenar Tabla
+  //-------------------------------------------------------
+  static llenarTablaDatos(datos) {
+    // Obtener el cuerpo de la tabla
+    const cuerpo = document.getElementById("tablaResultados");
+
+    // Limpiar el cuerpo de la tabla (por si ya tiene datos)
+    cuerpo.innerHTML = "";
+
+    // Agregar los datos al cuerpo de la tabla
+    datos.forEach((dato) => {
+      const fila = document.createElement("tr");
+      fila.innerHTML = `
+      <td>${dato.id}</td>
+      <td>${dato.nombre}</td>
+      <td>${dato.apellido}</td>
+      <td>${dato.edad}</td>
+    `;
+      cuerpo.appendChild(fila);
+    });
   }
 }
--- script.js
+++ script.js
@@ -2,22 +2,34 @@
 import { Libreria } from "./libreria.js";
 
 // -------------------------------------------
+// CREACIÓN DE ELEMENTOS DEL DOM
+// -------------------------------------------
+// Creo la Tabla
+Libreria.crearTablaDatos(
+  "tablaDatos",
+  "USUARIOS REGISTRADOS",
+  ["ID", "NOMBRE", "APELLIDO", "EDAD"],
+  "body"
+);
+
+// -------------------------------------------
+// REFERENCIAS A ELEMENTOS DEL DOM
+// -------------------------------------------
+const getEl = (id) => document.getElementById(id);
+const tablaDatos = getEl('tablaDatos'); 
+
+// -------------------------------------------
 // PROGRAMA PRINCIPAL
 // -------------------------------------------
-// Datos de ejemplo para llenar la tabla
-const params = {
-  id: "tablaDatos",
-  padre: "body",
-  tituloTabla: "USUARIOS REGISTRADOS",
-  campos: ["ID", "NOMBRE", "APELLIDO", "EDAD"],
-  datos: [
-    { id: 1, nombre: "Juan", apellido: "Perez", edad: 20 },
-    { id: 2, nombre: "María", apellido: "Gómez", edad: 34 },
-    { id: 3, nombre: "Pedro", apellido: "Martínez", edad: 45 },
-    { id: 4, nombre: "Kelly", apellido: "Jaramillo", edad: 22 },
-    { id: 5, nombre: "Miguel", apellido: "Gallo", edad: 40},
-  ],
-};
+// Datos de Prueba para llenar la tabla
+const datos = [
+  { id: 1, nombre: "Juan", apellido: "Perez", edad: 20 },
+  { id: 2, nombre: "María", apellido: "Gómez", edad: 34 },
+  { id: 3, nombre: "Pedro", apellido: "Martínez", edad: 45 },
+  { id: 4, nombre: "Kelly", apellido: "Jaramillo", edad: 22 },
+  { id: 5, nombre: "Miguel", apellido: "Gallo", edad: 40 },
+];
 
-// Creación de la Tabla
-Libreria.crearTablaDatos(params);
+// Llenado de Tabla con Datos de Prueba
+Libreria.llenarTablaDatos(datos);
+
